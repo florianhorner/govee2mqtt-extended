a803f9f1cf89cf3c8867bb94ce457bec01563b13
Florian Horner <florian@horner.io>
2026-04-21 18:31:32 +0200

    fix: scope changelog to fork commits only, fix issue URLs

--- scripts/tag-release.sh
+++ scripts/tag-release.sh
@@ -4,6 +4,11 @@
 TAG_NAME=${TAG_NAME:-$(git -c "core.abbrev=8" show -s "--format=%cd-%h" "--date=format:%Y.%m.%d")}
 git tag $TAG_NAME
 ./scripts/apply-tag.sh
-docker run -t -v "$(pwd)":/app/ "ghcr.io/orhun/git-cliff/git-cliff:${TAG:-latest}" -o addon/CHANGELOG.md -c scripts/cliff.toml
+docker run -t \
+  -v "$(git rev-parse --git-common-dir):/git-common" \
+  -v "$(pwd)":/app/ \
+  -e GIT_DIR=/git-common \
+  "ghcr.io/orhun/git-cliff/git-cliff:${TAG:-latest}" \
+  --latest -o /app/addon/CHANGELOG.md -c /app/scripts/cliff.toml
 git add addon/config.yaml addon/CHANGELOG.md
 git commit -m "Tag $TAG_NAME"
